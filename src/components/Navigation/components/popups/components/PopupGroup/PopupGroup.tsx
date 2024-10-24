9bc177dbe828a62d60d6f5b0d7bc3ca39becd100
0viliniw0 <bklaj98@gmail.com>
2024-10-24 19:29:44 +0400

    fix: refactor

--- src/components/Navigation/components/popups/components/PopupGroup/PopupGroup.tsx
+++ src/components/Navigation/components/popups/components/PopupGroup/PopupGroup.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, {useMemo} from 'react';
 
 import {Col, GridColumnSizesType, Row} from '@gravity-ui/page-constructor';
 
@@ -14,22 +14,36 @@ const b = block('popup-group');
 type PopupGroupProps = CategoryGroupData & {
     section?: string;
     sizes?: GridColumnSizesType;
+    withFixItems?: boolean;
 };
 
-export const PopupGroup = ({sizes, section, ...group}: PopupGroupProps) => (
-    <div className={b()}>
-        <Row>
-            <Col className={b('title')}>
-                <PopupTitle {...group} />
-            </Col>
-        </Row>
-        <div>
-            <NavigationItemsList
-                items={group.items}
-                section={section}
-                sizes={sizes}
-                className={b('items')}
-            />
+export const PopupGroup = ({sizes, section, withFixItems, ...group}: PopupGroupProps) => {
+    const items = useMemo(
+        () =>
+            group.items.map((item) => ({
+                ...item,
+                description: group.showItemDescriptions === 'no' ? undefined : item.description,
+                icon: group.showItemIcons === 'no' ? undefined : item.icon,
+                image: group.showItemIcons === 'no' ? undefined : item.image,
+            })),
+        [group.items, group.showItemDescriptions, group.showItemIcons],
+    );
+
+    return (
+        <div className={b()} style={{backgroundColor: group.backgroundColor}}>
+            <Row>
+                <Col className={b('title')}>
+                    <PopupTitle {...group} />
+                </Col>
+            </Row>
+            <div>
+                <NavigationItemsList
+                    items={items}
+                    section={section}
+                    sizes={sizes}
+                    className={withFixItems ? [b('items'), b('items_fix')].join(' ') : b('items')}
+                />
+            </div>
         </div>
-    </div>
-);
+    );
+};
